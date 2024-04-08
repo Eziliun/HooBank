b69a160ebd4a1b44c156efc05a32b391bf6d2ed9
Wilian Dimitri <wilianw2011@hotmail.com>
2024-04-08 17:57:45 -0300

    Refatoracoes no codigo, conclusao de Stats, Billing, Business e inicio de Testimonials

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 
 
-const Button = () => {
+const Button = ({styles}) => {
   return (
-    <div>
-      
-    </div>
+<button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+    Get Started
+</button>
   )
 }
 
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { feedback } from '../constants'
+import styles from '../style'
+import FeedBackCard from './FeedbackCard'
 
-const Testimonials = () => {
-  return (
-    <div>
+const Testimonials = () => (
+    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
+      <div />
+
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:'></div>
       
-    </div>
+    </section>
   )
-}
 
 export default Testimonials
